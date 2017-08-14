e99a5e75a46ab63430f9a3c89ec8c1a60eb52b15
Veselin Petkov <vpetkov@me.com>
2017-08-14 19:14:53 +0300

    Add ackrc to link script

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -2,4 +2,4 @@ ln -sf Documents/Terminal/bashrc.txt ~/.bash_profile
 ln -sf Documents/Terminal/gitconfig.txt ~/.gitconfig
 ln -sf Documents/Terminal/inputrc.txt ~/.inputrc
 ln -sf Documents/Terminal/vimrc.txt ~/.vimrc
-
+ln -sf Documents/Terminal/ackrc.txt ~/.ackrc
