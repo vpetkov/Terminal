2b0735fa0b6b0254b1ce929368f2f44f2a8854ad
Veselin Petkov <vpetkov@me.com>
2017-10-09 13:06:09 +0300

    Add irbrc and add qt5 to the path

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -3,3 +3,5 @@ ln -sf Documents/Terminal/gitconfig.txt ~/.gitconfig
 ln -sf Documents/Terminal/inputrc.txt ~/.inputrc
 ln -sf Documents/Terminal/vimrc.txt ~/.vimrc
 ln -sf Documents/Terminal/ackrc.txt ~/.ackrc
+ln -sf Documents/Terminal/irbrc.txt ~/.irbrc
+touch ~/.irb_history
