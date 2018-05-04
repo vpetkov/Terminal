ddf87bd0a877213b5f7a217a952801b9ffab2e2e
Veselin Petkov <vpetkov@me.com>
2018-05-04 15:03:46 +0300

    Add new min-vimrc
    vim8 comaptible

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,7 +1,7 @@
 ln -sf Documents/Terminal/bashrc.txt ~/.bash_profile
 ln -sf Documents/Terminal/gitconfig.txt ~/.gitconfig
 ln -sf Documents/Terminal/inputrc.txt ~/.inputrc
-ln -sf Documents/Terminal/vimrc.txt ~/.vimrc
+ln -sf Documents/Terminal/min-vimrc.txt ~/.vimrc
 ln -sf Documents/Terminal/ackrc.txt ~/.ackrc
 ln -sf Documents/Terminal/irbrc.txt ~/.irbrc
 touch ~/.irb_history
