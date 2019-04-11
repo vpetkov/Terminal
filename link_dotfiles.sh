b6f1121b29eb1a5d1dd8e666472da7976abb7ffb
Veselin Petkov <vpetkov@me.com>
2019-04-11 14:23:27 +0300

    Add gemrc

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -5,5 +5,5 @@ ln -sf ~/Documents/Terminal/vimrc.txt ~/.vimrc
 ln -sf ~/Documents/Terminal/vim ~/.vim
 ln -sf ~/Documents/Terminal/ackrc.txt ~/.ackrc
 ln -sf ~/Documents/Terminal/irbrc.txt ~/.irbrc
-ln -sf ~/Documents/Terminal/vim/vim-pathogen/autoload ~/Documents/Terminal/vim/autoload
+ln -sf ~/Documents/Terminal/gemrc.txt ~/.gemrc
 touch ~/.irb_history
